83b39bff6c9856f0a714aa0872d6a4fc73e52bba
Alessandro Clerici <45131494+sgorblex@users.noreply.github.com>
2023-12-27 19:08:20 +0100

    add LICENSE

--- birthdays_I_care_about.py
+++ birthdays_I_care_about.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python3
 
+# Copyright (C) 2023 Alessandro "Sgorblex" Clerici Lorenzini
+#
+# This file is part of birthdays_I_care_about.
+#
+# birthdays_I_care_about is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# birthdays_I_care_about is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with birthdays_I_care_about. If not, see <https://www.gnu.org/licenses/>.
+
+
 from datetime import datetime
 import os.path
 from sys import argv
